f6622615ac9404470fe41aacc05d4367590ecc2c
StUser4pda <84583436+StUser4pda@users.noreply.github.com>
2021-05-21 20:52:13 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -14,11 +14,17 @@ filtrite() {
     echo "::group::List: $1"
     log "Start generating $1"
     ./filtrite "lists/$1.txt" "rules/$1.dat" "logs/$1.log"
+    sleep 5
     ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files="rules/$1.dat" --output_file="rules/$1_b1.txt" > "logs/$1_err2.log" 2>&1
+    sleep 5
     sort -u "rules/$1_b1.txt" > "rules/$1_b2.txt"
+    sleep 5
     perl -E "while(<>) { print $_ unless (/@@/ or /\#/ or /%%/ or /\#\?\#/ ); }" "rules/$1_b2.txt" > "rules/$1_b3.txt"
+    sleep 5
     perl -E "while(<>) { print $_ if (/@@/ and !/\#\?\#/); }" "rules/$1_b2.txt" > "rules/$1_b4.txt"
+    sleep 5
     ./deps/ruleset_converter --input_format=filter-list --output_format=unindexed-ruleset --input_files="rules/$1_b3.txt","rules/$1_b4.txt" --output_file="dist/$1.dat" > "logs/$1_err3.log" 2>&1
+    sleep 5
     ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files="dist/$1.dat" --output_file="dist/$1.txt" > "logs/$1_err4.log" 2>&1
     echo "::endgroup::"
 }
