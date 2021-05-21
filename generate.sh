60315da6db391eea1017418de852030b9dd500e6
StUser4pda <84583436+StUser4pda@users.noreply.github.com>
2021-05-21 19:49:03 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -12,18 +12,18 @@ filtrite() {
     echo "::group::List: $1"
     log "Start generating $1"
     ./filtrite "lists/$1.txt" "rules/$1.dat" "logs/$1.log"
-    log "Step 2"
-    ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files=rules/$1.dat --output_file=rules/$1_b1.txt 2> logs/$1_err2.log
-    log "Step 3"
-    sort -u rules/$1_b1.txt > rules/$1_b2.txt
-    log "Step 4"
-    perl -E "while(<>) { print $_ unless (/@@/ or /\#/ or /%%/ or /\#\?\#/ ); }" rules/$1_b2.txt > rules/$1_b3.txt
-    log "Step 5"
-    perl -E "while(<>) { print $_ if (/@@/ and !/\#\?\#/); }" rules/$1_b2.txt > rules/$1_b4.txt
-    log "Step 6"
-    ./deps/ruleset_converter --input_format=filter-list --output_format=unindexed-ruleset --input_files=rules/$1_b3.txt,rules/$1_b4.txt --output_file=dist/$1 2> logs/$1_err3.log
-    log "Step 7"
-    ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files=dist/$1.dat --output_file=dist/$1.txt 2> logs/$1_err4.log
+    echo "Step 2"
+    ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files="rules/$1.dat" --output_file="rules/$1_b1.txt" > "logs/$1_err2.log" 2>&1
+    echo "Step 3"
+    sort -u "rules/$1_b1.txt" > "rules/$1_b2.txt"
+    echo "Step 4"
+    perl -E "while(<>) { print $_ unless (/@@/ or /\#/ or /%%/ or /\#\?\#/ ); }" "rules/$1_b2.txt" > "rules/$1_b3.txt"
+    echo "Step 5"
+    perl -E "while(<>) { print $_ if (/@@/ and !/\#\?\#/); }" "rules/$1_b2.txt" > "rules/$1_b4.txt"
+    echo "Step 6"
+    ./deps/ruleset_converter --input_format=filter-list --output_format=unindexed-ruleset --input_files="rules/$1_b3.txt","rules/$1_b4.txt" --output_file="dist/$1" > "logs/$1_err3.log" 2>&1
+    echo "Step 7"
+    ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files="dist/$1.dat" --output_file="dist/$1.txt" > "logs/$1_err4.log" 2>&1
     echo "::endgroup::"
 }
 
@@ -58,7 +58,7 @@ echo "::group::List: bromite-default"
 # Download default bromite filter list
 wget -O rules/filters.dat https://www.bromite.org/filters/filters.dat
 log "Start generating bromite-default filters.txt"
-./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files=rules/filters.dat --output_file=rules/filters.txt 2> logs/filter.log
+./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files=rules/filters.dat --output_file=rules/filters.txt > logs/filter.log 2>&1
 echo "::endgroup::"
 
 # All other lists can be listed here
