5d83d690e5d1789121261036650b00223ef6f7aa
StUser4pda <84583436+StUser4pda@users.noreply.github.com>
2021-05-22 10:59:34 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -53,7 +53,6 @@ echo "::endgroup::"
 echo "::group::List: bromite-default"
 # Download default bromite filter list
 wget -O logs/filters.dat https://www.bromite.org/filters/filters.dat
-# log "Start generating bromite-default filters.txt"
 # ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files=rules/filters.dat --output_file=rules/filters.txt > logs/filter.log 2>&1
 echo "::endgroup::"
 
