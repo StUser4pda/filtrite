22dd10653bc90a1b0613a97d20489ae022dbe157
StUser4pda <84583436+StUser4pda@users.noreply.github.com>
2021-05-22 11:42:05 +0300

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -14,13 +14,28 @@ filtrite() {
     echo "::group::List: $1"
     log "Start generating $1"
     ./filtrite "lists/$1.txt" "logs/$1.dat" "logs/$1.log"
+
+    # Joining our lists with the default filters.dat
     ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files="logs/$1.dat","logs/filters.dat" --output_file="logs/$1_b0.txt" > "logs/$1_2.log" 2>&1
+
+    # Removing duplicate values (a ruleset_converter bug?) like :
+    # $script,xmlhttprequest,third-party,domain=tamilyogi.cc|tamilyogi.vip
+    # $script,xmlhttprequest,third-party,domain=tamilyogi.vip|tamilyogi.cc
+    ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files="logs/$1_b0.txt" --output_file="logs/$1_b0.dat" 
+    ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files="logs/$1_b0.dat" --output_file="logs/$1_b0.txt" 
+
+    # Fixing/sorting (I prefer sorted lists with whitelist rules at the end)
     grep -vf rules/badfilters.txt "logs/$1_b0.txt" > "logs/$1_b1.txt"
     sort -u "logs/$1_b1.txt" > "logs/$1_b2.txt"
     grep -v '^@@' "logs/$1_b2.txt" > "logs/$1_b3.txt"
     grep '^@@' "logs/$1_b2.txt" > "logs/$1_b4.txt"
+    
+    # Joining with myfilters.txt
     ./deps/ruleset_converter --input_format=filter-list --output_format=unindexed-ruleset --input_files="logs/$1_b3.txt","logs/$1_b4.txt","rules/myfilters.txt" --output_file="dist/$1.dat" > "logs/$1_3.log" 2>&1
+    
+    # Generating a filter list (*.txt) too
     ./deps/ruleset_converter --input_format=unindexed-ruleset --output_format=filter-list --input_files="dist/$1.dat" --output_file="dist/$1.txt" > "logs/$1_err4.log" 2>&1
+    
     echo "::endgroup::"
 }
 
